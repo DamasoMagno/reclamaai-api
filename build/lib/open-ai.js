46e547328a6475d1c4fb32f21cec242849ac61d9
DamasoMagno <limamdamaso@gmail.com>
2025-11-24 21:29:17 -0300

    feat: removed build folder

--- build/lib/open-ai.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.openai = void 0;
-const openai_1 = require("openai");
-const env_1 = require("../env");
-const openai = new openai_1.OpenAI({
-    apiKey: env_1.env.OPENAI_API_KEY,
-});
-exports.openai = openai;
